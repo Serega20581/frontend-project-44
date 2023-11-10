31ee301321b23c51e89dd04d929ea2e8c08f3528
Sergei <toptunov.2005@mail.ru>
2023-11-10 09:46:21 +0300

    update

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { a } from "../src/cli";
-
+import { a } from "../src/cli.js";
+a();
 console.log('Welcome to the Brain Games!');
--- src/cli.js
+++ src/cli.js
@@ -4,5 +4,4 @@ console.log('Welcome to the Brain Games!');
 var userName = readlineSync.question('May I have your name? ');
 console.log('Hello, ' + userName + '!');
 }
-a();
 export {a};
