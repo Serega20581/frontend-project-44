33395e0e15e42a1656622f6eb4e66b81246b70ca
Sergei <toptunov.2005@mail.ru>
2024-03-11 10:39:53 +0300

    fixesssss

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -6,7 +6,7 @@ function isPrime(number) {
     return false;
   }
 
-  for (let i = 2; i < Math.sqrt(number); i += 2) {
+  for (let i = 2; i < Math.sqrt(number); i++) {
     if (number % i === 0) {
       return false;
     }
